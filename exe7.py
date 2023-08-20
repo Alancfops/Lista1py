7d2185f8895c56015056e344a90d9627b001cc2e
Alancfops <fariasalan13@gmail.com>
2023-08-20 16:37:14 -0300

    exe7 lista1

---
+++ exe7.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite seu numero:"))
+n2 = float(input("Digite seu outro numero:"))
+print("Seus numeros foram {} e {}".format(n1, n2))
+if n1 > n2:
+    print("{} ,esse numero foi maior!".format(n1))
+else:
+    print("{} ,esse numero foi maior!".format(n2))
